cf7fdbec5ff967bb85de0b5761d85c5d5119cc33
ava krishn <avakrishn@gmail.com>
2018-06-17 19:04:18 -0700

    added pseudocode for javascript and jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
 // random number to match score when game starts (19 - 120)
 
 //hidden values of crystals (1 - 12)
+
+//when click on crystal, update the player's score counter
+
+// The player wins if their total score matches the random number from the beginning of the game
+
+//The player loses if their score goes above the random number
+
+//The game restarts whenever the player wins or loses.(restart function)
+    //When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. The user's score (and score counter) will reset to zero.
