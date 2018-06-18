6741126e6b43fe28865d498d779f26113d6e911e
ava krishn <avakrishn@gmail.com>
2018-06-18 16:18:14 -0700

    add css styling to resultWin and resultLose and changed all colors to hexadecimal html color codes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,39 +8,6 @@ var loses = 0;
 var result;
 
 
-// function showInstructions() {
-//     $('.instruct').hover(
-//         function (){
-//             $('.instReveal').show().fadeIn("slow");
-//         },
-//         function (){
-//             $('.instReveal').hide().fadeOut("slow"); 
-//     });
-
-//     $('.instReveal').hover(function(){
-//         $(this).show();
-//     });
-
-    
-// }
-
-// function showInstructions() {
-//     $('.instruct').hover(
-//         function (){
-//             $('.instReveal').show().fadeIn("slow");
-//         },
-//         function (){
-//             $('.instReveal').hide().fadeOut("slow"); 
-//     }
-// );
-
-//     $('.instReveal').hover(function(){
-//         $(this).show();
-//     });
-
-    
-// }
-
 function restart(){
     //new random number
     ranNum = Math.floor(Math.random() * 102) + 19; 
@@ -111,19 +78,3 @@ $(document).on('click', 'button', function(){
    
 });
 
-
-
-
-
-//   X random number to match score when game starts (19 - 120)
-
-// X hidden values of crystals (1 - 12)
-
-// X when click on crystal, update the player's score counter
-
-// X The player wins if their total score matches the random number from the beginning of the game
-
-// XThe player loses if their score goes above the random number
-
-// X The game restarts whenever the player wins or loses.(restart function)
-    // X When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. The user's score (and score counter) will reset to zero.
